a267bd8bc703c5f10f391249bc5de18e33fdfd3c
Xaboule <xaboule@gmail.com>
2022-07-19 14:10:01 +0200

    ouuuuui

---
+++ src/grid.js
@@ -0,0 +1,55 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+import * as CANNON from 'cannon-es';
+import CannonDebugger from 'cannon-es-debugger';
+
+const Grid = (world, position) => {
+    
+  let arr = []
+    const defaultMaterial = new CANNON.Material('default');
+    const defaultContactMaterial = new CANNON.ContactMaterial(
+      defaultMaterial,
+      defaultMaterial,
+      {
+        friction: 0.1,
+        restitution: 0.0,
+      }
+      );
+      world.addContactMaterial(defaultContactMaterial);
+
+      
+      const createBorder = (position, quaternion) => {
+   
+    
+      
+        // Cannon.js body
+        const sizeX = 4
+        const sizeY = 3
+        const sizeZ = 0.2
+        const halfExtents = new CANNON.Vec3(sizeX, sizeY, sizeZ)
+        const planeShape = new CANNON.Box(halfExtents);
+        
+        const planeBody = new CANNON.Body({
+          mass: 0,
+          position: position,
+          shape: planeShape,
+          material: defaultMaterial,
+          type: 2,
+        });
+        planeBody.addShape(planeShape);
+        // body.position.copy(mesh.position)
+        // console.log('bodyyyy', body.position)
+        world.addBody(planeBody);
+        console.log('yiiiii', planeBody);
+        arr.push(planeBody)
+      };
+      
+    
+  createBorder(new CANNON.Vec3(...position));
+  console.log(arr)
+};
+
+
+export default Grid;
--- src/script.js
+++ src/script.js
@@ -4,7 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'lil-gui';
 import * as CANNON from 'cannon-es';
 import CannonDebugger from 'cannon-es-debugger';
-
+import Grid from './grid.js';
+import Grid90 from './grid90';
 
 /**
  * Base
@@ -20,9 +21,6 @@ const scene = new THREE.Scene();
 
 const gridHelper = new THREE.GridHelper(8, 4);
 scene.add(gridHelper);
-
-
-
 
 const raycaster = new THREE.Raycaster();
 //lights
@@ -42,186 +40,174 @@ scene.add(plane1);
 const allCells = new THREE.Group();
 scene.add(allCells);
 
-const cubeGeo = new THREE.BoxBufferGeometry(1.8, 4, 1.8);
+const cubeGeo = new THREE.BoxBufferGeometry(1.8, 6, 1.8);
 const cubeMat = new THREE.MeshBasicMaterial({
-    color: '#ffffff',
-    transparent: true,
-    opacity: 0.5,
+  color: '#ffffff',
+  transparent: true,
+  opacity: 0.5,
 });
 
-let cellsToTest = []
+let cellsToTest = [];
 ////////////////////1ST ROW
 const cube1 = new THREE.Mesh(cubeGeo, cubeMat);
-cube1.position.set(-3, 1, 3);
+cube1.position.set(-3, 2, 3);
 allCells.add(cube1);
-cellsToTest.push(cube1)
-
+cellsToTest.push(cube1);
 
 const cube2 = new THREE.Mesh(cubeGeo, cubeMat);
-cube2.position.set(-1, 1, 3);
+cube2.position.set(-1, 2, 3);
 allCells.add(cube2);
-cellsToTest.push(cube2)
+cellsToTest.push(cube2);
 
 const cube3 = new THREE.Mesh(cubeGeo, cubeMat);
-cube3.position.set(1, 1, 3);
+cube3.position.set(1, 2, 3);
 allCells.add(cube3);
-cellsToTest.push(cube3)
-
-
+cellsToTest.push(cube3);
 
 const cube4 = new THREE.Mesh(cubeGeo, cubeMat);
-cube4.position.set(3, 1, 3);
+cube4.position.set(3, 2, 3);
 allCells.add(cube4);
-cellsToTest.push(cube4)
+cellsToTest.push(cube4);
 
 /////////////// 2ND ROW
 const cube5 = new THREE.Mesh(cubeGeo, cubeMat);
-cube5.position.set(-3, 1, 1);
+cube5.position.set(-3, 2, 1);
 allCells.add(cube5);
-cellsToTest.push(cube5)
+cellsToTest.push(cube5);
 
 const cube6 = new THREE.Mesh(cubeGeo, cubeMat);
-cube6.position.set(-1, 1, 1);
+cube6.position.set(-1, 2, 1);
 allCells.add(cube6);
-cellsToTest.push(cube6)
+cellsToTest.push(cube6);
 
 const cube7 = new THREE.Mesh(cubeGeo, cubeMat);
-cube7.position.set(1, 1, 1);
+cube7.position.set(1, 2, 1);
 allCells.add(cube7);
-cellsToTest.push(cube7)
+cellsToTest.push(cube7);
 
 const cube8 = new THREE.Mesh(cubeGeo, cubeMat);
-cube8.position.set(3, 1, 1);
+cube8.position.set(3, 2, 1);
 allCells.add(cube8);
-cellsToTest.push(cube8)
+cellsToTest.push(cube8);
 ///////////////////////////3RD ROW
 
 const cube9 = new THREE.Mesh(cubeGeo, cubeMat);
-cube9.position.set(-3, 1, -1);
+cube9.position.set(-3, 2, -1);
 allCells.add(cube9);
-cellsToTest.push(cube9)
+cellsToTest.push(cube9);
 
 const cube10 = new THREE.Mesh(cubeGeo, cubeMat);
-cube10.position.set(-1, 1, -1);
+cube10.position.set(-1, 2, -1);
 allCells.add(cube10);
-cellsToTest.push(cube10)
+cellsToTest.push(cube10);
 
 const cube11 = new THREE.Mesh(cubeGeo, cubeMat);
-cube11.position.set(1, 1, -1);
+cube11.position.set(1, 2, -1);
 allCells.add(cube11);
-cellsToTest.push(cube11)
+cellsToTest.push(cube11);
 
 const cube12 = new THREE.Mesh(cubeGeo, cubeMat);
-cube12.position.set(3, 1, -1);
+cube12.position.set(3, 2, -1);
 allCells.add(cube12);
-cellsToTest.push(cube12)
-
+cellsToTest.push(cube12);
 
 //////////////////////4TH ROW
 
 const cube13 = new THREE.Mesh(cubeGeo, cubeMat);
-cube13.position.set(-3, 1, -3);
+cube13.position.set(-3, 2, -3);
 allCells.add(cube13);
-cellsToTest.push(cube13)
+cellsToTest.push(cube13);
 
 const cube14 = new THREE.Mesh(cubeGeo, cubeMat);
-cube14.position.set(-1, 1, -3);
+cube14.position.set(-1, 2, -3);
 allCells.add(cube14);
-cellsToTest.push(cube14)
+cellsToTest.push(cube14);
 
 const cube15 = new THREE.Mesh(cubeGeo, cubeMat);
-cube15.position.set(1, 1, -3);
+cube15.position.set(1, 2, -3);
 allCells.add(cube15);
-cellsToTest.push(cube15)
+cellsToTest.push(cube15);
 
 const cube16 = new THREE.Mesh(cubeGeo, cubeMat);
-cube16.position.set(3, 1, -3);
+cube16.position.set(3, 2, -3);
 allCells.add(cube16);
-cellsToTest.push(cube16)
+cellsToTest.push(cube16);
 
-console.log(cellsToTest)
+console.log(cellsToTest);
 /**
  * Objects
  */
-let played = 0
+let played = 0;
 
+const objectsToUpdate = [];
 
-
-const objectsToUpdate = []
-
-
-const world = new CANNON.World()
-world.gravity.set(0,-9.82,0)
+const world = new CANNON.World();
+world.gravity.set(0, -14.82, 0);
 
 const cannonDebugger = new CannonDebugger(scene, world, {
-    // options...
-     color : 0x00ff00, scale : 1
-  })
+  // options...
+  color: 0x00ff00,
+  scale: 1,
+});
 
-
-
-const defaultMaterial = new CANNON.Material('default')
+const defaultMaterial = new CANNON.Material('default');
 const defaultContactMaterial = new CANNON.ContactMaterial(
-    defaultMaterial,
-    defaultMaterial,
-    {
-        friction: 0.1,
-        restitution: 0.7
-    }
-)
-world.addContactMaterial(defaultContactMaterial)
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.3,
+  }
+);
+world.addContactMaterial(defaultContactMaterial);
 
-const floorBody = new CANNON.Body()
-const floorShape = new CANNON.Plane(new CANNON.Vec3(10,10))
-floorBody.material = defaultContactMaterial
-floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(- 1, 0, 0), Math.PI * 0.5)
-floorBody.position.y = -1
-floorBody.mass = 0
-floorBody.addShape(floorShape)
-world.addBody(floorBody)
+const floorBody = new CANNON.Body();
+const floorShape = new CANNON.Plane(new CANNON.Vec3(10, 10));
+floorBody.material = defaultContactMaterial;
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+floorBody.position.y = -1;
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+world.addBody(floorBody);
 
-const radius = 0.8
+const radius = 0.7;
 
-const createSphere = (radius, position) =>
-{
-    // Three.js mesh
-    const mesh = new THREE.Mesh(
-        new THREE.SphereGeometry(radius, 20, 20),
-        new THREE.MeshStandardMaterial({
-            color : played%2 != 0 ? '#ff0000' : '#00ff00',
-            metalness: 0.3,
-            roughness: 0.4
-            // envMap: environmentMapTexture,
-            // envMapIntensity: 0.5
-        })
-    )
-    mesh.castShadow = true
-    mesh.position.copy(position)
-    scene.add(mesh)
-
-
-    // Cannon.js body
-    const shape = new CANNON.Sphere(radius)
-
-    const body = new CANNON.Body({
-        mass: 1,
-        // position: new CANNON.Vec3(0, 3, 0),
-        position: position,
-        shape: shape,
-        material: defaultMaterial
+const createSphere = (radius, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(
+    new THREE.SphereGeometry(radius, 20, 20),
+    new THREE.MeshStandardMaterial({
+      color: played % 2 != 0 ? '#ff0000' : '#00ff00',
+      metalness: 0.3,
+      roughness: 0.4,
+      // envMap: environmentMapTexture,
+      // envMapIntensity: 0.5
     })
-    body.addShape(shape)
-    // body.position.copy(mesh.position)
-    console.log('meshhhh',mesh.position)
-    console.log('bodyyyy', body.position)
-    mesh.position.copy(body.position)
-    world.addBody(body)
-    objectsToUpdate.push({mesh, body})
+  );
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
 
- 
-}
+  // Cannon.js body
+  const shape = new CANNON.Sphere(radius);
 
-console.log(world)
+  const body = new CANNON.Body({
+    mass: 1,
+    // position: new CANNON.Vec3(0, 3, 0),
+    position: position,
+    shape: shape,
+    material: defaultMaterial,
+  });
+  body.addShape(shape);
+  // body.position.copy(mesh.position)
+  console.log('meshhhh', mesh.position);
+  console.log('bodyyyy', body.position);
+  mesh.position.copy(body.position);
+  world.addBody(body);
+  objectsToUpdate.push({ mesh, body });
+};
+
+console.log(world);
 //  createSphere(0.8, { x: 0, y: 3, z: 0 })
 // const rayOrigin = new THREE.Vector3(-3, 0, 0);
 // const rayDirection = new THREE.Vector3(10, 0, 0);
@@ -235,12 +221,18 @@ console.log(world)
 // const intersects = raycaster.intersectObjects([object1, object2, object3]);
 // console.log(intersects);
 
-const debugObject = {}
-debugObject.createSphere = () =>
-{
-    createSphere(radius, position)
-}
-scene.add(debugObject, 'createSphere')
+const debugObject = {};
+// debugObject.createSphere = () =>
+// {
+//     createSphere(radius, position)
+// }
+// scene.add(debugObject, 'createSphere')
+
+// debugObject.createBorder = () =>
+// {
+//     createBorder(radius, position)
+// }
+// scene.add(debugObject, 'createBorder')
 /**
  * Sizes
  */
@@ -294,17 +286,37 @@ document.addEventListener('mousemove', onMouseMove);
 // document.addEventListener('mousedown', onMouseDown)
 // document.addEventListener('mouseup', onMouseUp)
 
-let clicked = 0
-function onMouseDown(event){
-    console.log("mousedown")
-    event.preventDefault()
-        clicked += 1
-        setTimeout(()=> {clicked = 0}, '7')
-    }
-    function onMouseUp(event){
-        console.log("mouseup")
-            clicked = 0
-        }
+
+Grid(world, [0, 2, 0]);
+Grid(world, [0, 2, 2]);
+Grid(world, [0, 2, -2]);
+Grid(world, [0, 2, -4]);
+Grid(world, [0, 2, 4]);
+Grid90(world, [-4, 2, 0]);
+Grid90(world, [-2, 2, 0]);
+Grid90(world, [0, 2, 0]);
+Grid90(world, [2, 2, 0]);
+Grid90(world, [4, 2, 0]);
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(10, 10),
+  new THREE.MeshBasicMaterial({ color: '#fffff' })
+);
+// scene.add(plane)
+
+let clicked = 0;
+function onMouseDown(event) {
+  console.log('mousedown');
+  event.preventDefault();
+  clicked += 1;
+  setTimeout(() => {
+    clicked = 0;
+  }, '7');
+}
+function onMouseUp(event) {
+  console.log('mouseup');
+  clicked = 0;
+}
 function onMouseMove(event) {
   event.preventDefault();
 
@@ -316,61 +328,54 @@ function onMouseMove(event) {
  * Animate
  console.log(gridHelper);
  */
- const clock = new THREE.Clock();
- let oldElapsedTime = 0
+const clock = new THREE.Clock();
+let oldElapsedTime = 0;
 
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
 
-
- const tick = () =>
- {
-     const elapsedTime = clock.getElapsedTime()
-     const deltaTime = elapsedTime - oldElapsedTime
-     oldElapsedTime = elapsedTime
-    
-    controls.update();
+  controls.update();
   raycaster.setFromCamera(mouse, camera);
-  const intersection = raycaster.intersectObjects(
-cellsToTest
-  );
+  const intersection = raycaster.intersectObjects(cellsToTest);
   for (const object of cellsToTest) {
-      
-      object.material = cubeMat;
+    object.material = cubeMat;
   }
-  if ( intersection.length > 0 ) {
-      
-      
-      intersection[0].object.material = (new THREE.MeshBasicMaterial({color :'#ff0000'}));
-      // console.log(intersection)
+  if (intersection.length > 0) {
+    intersection[0].object.material = new THREE.MeshBasicMaterial({
+      color: '#ff0000',
+    });
+    // console.log(intersection)
+  }
+
+  window.addEventListener('mousedown', onMouseDown);
+  window.addEventListener('mouseup', onMouseUp);
+  if (intersection[0] !== undefined && clicked === 1) {
+    removeEventListener('mousedown', onMouseDown);
+    let pos = new THREE.Vector3(
+      intersection[0].object.position.x,
+      intersection[0].object.position.y + 5,
+      intersection[0].object.position.z
+    );
+    createSphere(radius, pos);
+    // console.log(body)
+    // setTimeout(()=>{},'100')
+    // console.log(createSphere(),)
+    played += 1;
+
+    if (clicked > 1 && clicked !== 0) {
+      clicked = 0;
+      setTimeout(() => {}, '500');
     }
+  } else null;
 
-   window.addEventListener('mousedown', onMouseDown)
-    window.addEventListener('mouseup', onMouseUp)
-    if(intersection[0] !== undefined && clicked === 1){
-
-        removeEventListener('mousedown', onMouseDown)
-        let pos = new THREE.Vector3(intersection[0].object.position.x,intersection[0].object.position.y + 3,intersection[0].object.position.z )
-        createSphere( radius,
-            pos)
-            // console.log(body)
-            // setTimeout(()=>{},'100')
-            // console.log(createSphere(),)
-            played += 1
-
-            if(clicked > 1 && clicked !== 0){
-                clicked = 0
-                setTimeout(()=>{},'1000')
-
-
-            }
-            
-        } else null
-
-    for (const object of objectsToUpdate){
-        object.mesh.position.copy(object.body.position)
-        object.mesh.quaternion.copy(object.body.quaternion)
-    }
-        world.step(1 / 60, deltaTime, 3)
-// scene.updateMatrixWorld()
+  for (const object of objectsToUpdate) {
+    object.mesh.position.copy(object.body.position);
+    object.mesh.quaternion.copy(object.body.quaternion);
+  }
+  world.step(1 / 60, deltaTime, 3);
+  // scene.updateMatrixWorld()
   // Update controls
   cannonDebugger.update();
   // Render
